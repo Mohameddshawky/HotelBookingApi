b7a317a0abeb4b699516c25d598f3d8ae371d889
mohamed ahmed shawky <147840781+Mohameddshawky@users.noreply.github.com>
2026-08-11 14:17:38 +0300

    add enum converter

--- HotelBookingApi/Program.cs
+++ HotelBookingApi/Program.cs
@@ -75,6 +75,7 @@ builder.Services.AddControllers()
     .AddJsonOptions(options =>
     {
         options.JsonSerializerOptions.Converters.Add(new HotelBookingApi.Converters.DateFormatConverter());
+        options.JsonSerializerOptions.Converters.Add(new System.Text.Json.Serialization.JsonStringEnumConverter());
     });
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
